97d66d22bcef261efbab4ef2845b885b33ad9a33
elementWebDev <ivinci.design@gmail.com>
2018-12-21 13:50:04 -0800

    add lesson 5
    contains previous lessons without distinction

---
+++ js/app.js
@@ -0,0 +1,44 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks",
+    image: "./img/vmSocks-green.png",
+    url: "./img/vmSocks-blue.png",
+    inStock: true,
+    inventory: 100,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./img/vmSocks-green.png"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./img/vmSocks-blue.png"
+      }
+    ],
+    sizes: ["small", "medium", "large"],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
+  }
+});
+
+/**
+ * Expression examples
+ *
+ *  {{ product + '?' }}
+ *  {{ firstName + ' ' + lastName }}
+ *  {{ clicked ? true : false }}
+ *  {{ message.split('').reverse().join('') }}
+ */
---
+++ notes.md
@@ -0,0 +1,15 @@
+## Shortcuts
+
+Because of the popularity of certain directives, there has been an effort to use shorthand for ease-of-use.
+
+### v-bind:
+- :alt="description"
+- :href="url"
+- :title="toolTip"
+- :class="isActive"
+- :style="isStyled"
+- :disabled="isDisabled"
+
+### v-on:
+- @click
+- @mouseover
